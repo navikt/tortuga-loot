108e31d104c9f67e344be8639beae26c4c268806
Marius Eriksen <Marius.Eriksen@nav.no>
2018-06-29 15:38:05 +0200

    Entity package no longer needed because loot will consume InntektSkatt instead of writing to popp DB

--- src/main/java/no/nav/opptjening/loot/entity/PersonNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.opptjening.loot.entity;
-
-public class PersonNotFoundException extends RuntimeException {
-
-    public PersonNotFoundException(String message) {
-        super(message);
-    }
-}
