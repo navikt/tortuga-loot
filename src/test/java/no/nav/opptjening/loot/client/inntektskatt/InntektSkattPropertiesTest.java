20fce1680a0c70c93b86fa73548c0e80c3fe5a00
Marius Eriksen <Marius.Eriksen@nav.no>
2018-10-05 14:06:29 +0200

    Added tests to InntektSkattProperties

---
+++ src/test/java/no/nav/opptjening/loot/client/inntektskatt/InntektSkattPropertiesTest.java
@@ -0,0 +1,35 @@
+package no.nav.opptjening.loot.client.inntektskatt;
+
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class InntektSkattPropertiesTest {
+
+    private static Map<String, String> applicationEnvironment = new HashMap<>();
+
+    @Test(expected = MissingClientConfig.class)
+    public void createFromEnvironment_throwsMissingClientConfig_when_INNTEKT_SKATT_URL_isNull() throws Exception {
+        applicationEnvironment.put("INNTEKT_SKATT_URL", null);
+        InntektSkattProperties.createFromEnvironment(applicationEnvironment);
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void createFromEnvironment_throwsURISyntaxException_when_INNTEKT_SKATT_URL_containsIllegalCharacters() throws Exception {
+        String malformedUrl = "https://www.malformedUrl .com";
+        applicationEnvironment.put("INNTEKT_SKATT_URL", malformedUrl);
+        InntektSkattProperties.createFromEnvironment(applicationEnvironment);
+    }
+
+    @Test
+    public void createFromEnvironment_setsInntektSkattPropertiesUrl_when_INNTEKT_SKATT_URL_isNotNull() throws Exception {
+        String urlToInntektSkattWs = "http://url_to_popp.com";
+        applicationEnvironment.put("INNTEKT_SKATT_URL", urlToInntektSkattWs);
+        InntektSkattProperties inntektSkattProperties = InntektSkattProperties.createFromEnvironment(applicationEnvironment);
+        assertEquals(urlToInntektSkattWs, inntektSkattProperties.getUrl());
+    }
+}
